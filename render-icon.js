644ba81c159a951c9eee965f70a39864dddf04cb
liixnglinb <lixingli1024@qq.com>
2026-09-16 22:43:06 +0800

    feat(brand): 账迹应用图标设计（渐变收据+对勾徽章）并更新 Android 启动图标

---
+++ render-icon.js
@@ -0,0 +1,15 @@
+﻿const { chromium } = require('playwright');
+const fs = require('fs');
+(async () => {
+  const svg = fs.readFileSync('brand/icon.svg', 'utf8');
+  const b = await chromium.launch();
+  for (const size of [512, 192]) {
+    const p = await b.newPage({ viewport: { width: size, height: size }, deviceScaleFactor: 1 });
+    await p.setContent('<body style="margin:0">' + svg.replace('<svg ', '<svg width="' + size + '" height="' + size + '" ') + '</body>');
+    await p.waitForTimeout(200);
+    await p.screenshot({ path: 'brand/icon-' + size + '.png', omitBackground: true });
+    await p.close();
+  }
+  await b.close();
+  console.log('rendered:', fs.readdirSync('brand').join(', '));
+})();
